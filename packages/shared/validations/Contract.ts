8a48b0bdb8bbd4d093cb5fe41e7b0354dfab1968
90sRehem <you@example.com>
2022-06-06 21:34:07 -0300

    chore: alterando nome dos métodos

--- packages/shared/validations/Contract.ts
+++ packages/shared/validations/Contract.ts
@@ -9,109 +9,109 @@ export class Contract extends Notifiable {
     return this;
   }
 
-  public IsNotNull(Value: any, Property: string, Message: string): Contract {
-    if (Value === null) {
-      this.Result(Property, Message);
+  public IsNotNull(value: any, property: string, message: string): Contract {
+    if (value === null) {
+      this.Result(property, message);
     }
     return this;
   }
 
-  public IsNotEmptyString(Value: string, Property: string, Message: string): Contract {
-    if (Value === "") this.Result(Property, Message);
+  public IsNotEmptyString(value: string, property: string, message: string): Contract {
+    if (value === "") this.Result(property, message);
     return this;
   }
 
-  public IsNotNullOrEmptyString(Value: string, Property: string, Message: string): Contract {
-    if (Value === "" || Value === null) this.Result(Property, Message);
+  public IsNotNullOrEmptyString(value: string, property: string, message: string): Contract {
+    if (value === "" || value === null) this.Result(property, message);
     return this;
   }
 
-  public AreEquals(Value1: any, Value2: any, Property: string, Message: string): Contract {
-    if (!(Value1 === Value2)) {
-      this.Result(Property, Message);
+  public AreEquals(value1: any, value2: any, property: string, message: string): Contract {
+    if (!(value1 === value2)) {
+      this.Result(property, message);
     }
     return this;
   }
 
   public IsBetweenMaxMin(
-    Value: number,
-    Max: number,
-    Min: number,
-    Property: string,
-    Message: string,
+    value: number,
+    max: number,
+    min: number,
+    property: string,
+    message: string,
   ): Contract {
-    if (Value <= Min || Value >= Max) {
-      this.Result(Property, Message);
+    if (value <= min || value >= max) {
+      this.Result(property, message);
     }
 
     return this;
   }
 
   public IsLengthBetweenMaxMin(
-    Value: any,
-    Max: number,
-    Min: number,
-    Property: string,
-    Message: string,
+    value: any,
+    max: number,
+    min: number,
+    property: string,
+    message: string,
   ): Contract {
-    if (Value) {
-      if (Value.length <= Min || Value.length >= Max) {
-        this.Result(Property, Message);
+    if (value) {
+      if (value.length <= min || value.length >= max) {
+        this.Result(property, message);
       }
     }
     return this;
   }
 
-  public IsLengthBiggerThan(Value: any, Min: number, Property: string, Message: string): Contract {
-    if (Value) {
-      if (Value.length > Min) this.Result(Property, Message);
+  public HasMaxLength(value: any, min: number, property: string, message: string): Contract {
+    if (value) {
+      if (value.length > min) this.Result(property, message);
     }
     return this;
   }
 
-  public IsLengthLessThan(Value: any, Max: number, Property: string, Message: string): Contract {
-    if (Value) {
-      if (Value.length < Max) this.Result(Property, Message);
+  public HasMinLength(value: any, max: number, property: string, message: string): Contract {
+    if (value) {
+      if (value.length < max) this.Result(property, message);
     }
     return this;
   }
 
-  public IsBiggerThan(Value: number, Min: number, Property: string, Message: string): Contract {
-    if (Value < Min) this.Result(Property, Message);
+  public IsBiggerThan(value: number, min: number, property: string, message: string): Contract {
+    if (value < min) this.Result(property, message);
 
     return this;
   }
 
-  public IsLessThan(Value: number, Max: number, Property: string, Message: string): Contract {
-    if (Value > Max) this.Result(Property, Message);
+  public IsLessThan(value: number, max: number, property: string, message: string): Contract {
+    if (value > max) this.Result(property, message);
 
     return this;
   }
 
-  public IsExactlyLength(Value: any, Size: number, Property: string, Message: string): Contract {
-    if (Value) {
-      const result = !!(Value !== null && Value !== "");
+  public IsExactlyLength(value: any, Size: number, property: string, message: string): Contract {
+    if (value) {
+      const result = !!(value !== null && value !== "");
       if (result) {
-        if (Value.length !== Size) this.Result(Property, Message);
+        if (value.length !== Size) this.Result(property, message);
       }
     }
     return this;
   }
 
-  public IsEmail(Value: string, Property: string, Message: string): Contract {
-    if (Value) {
+  public IsEmail(value: string, property: string, message: string): Contract {
+    if (value) {
       const result = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-        Value.toString(),
+        value.toString(),
       );
       if (!result) {
-        this.Result(Property, Message);
+        this.Result(property, message);
       }
     }
     return this;
   }
 
-  public IsUrl(Value: any, Property: string, Message: string): Contract {
-    if (Value) {
+  public IsUrl(value: any, property: string, message: string): Contract {
+    if (value) {
       const result = new RegExp(
         "^(https?:\\/\\/)?"
         + "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|"
@@ -120,16 +120,16 @@ export class Contract extends Notifiable {
         + "(\\?[;&a-z\\d%_.~+=-]*)?"
         + "(\\#[-a-z\\d_]*)?$",
         "i",
-      ).test(Value);
+      ).test(value);
       if (!result) {
-        this.Result(Property, Message);
+        this.Result(property, message);
       }
     }
     return this;
   }
 
-  public IsNotUrl(Value: any, Property: string, Message: string): Contract {
-    if (Value) {
+  public IsNotUrl(value: any, property: string, message: string): Contract {
+    if (value) {
       const result = new RegExp(
         "^(https?:\\/\\/)?"
         + "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|"
@@ -138,36 +138,36 @@ export class Contract extends Notifiable {
         + "(\\?[;&a-z\\d%_.~+=-]*)?"
         + "(\\#[-a-z\\d_]*)?$",
         "i",
-      ).test(Value);
+      ).test(value);
       if (result) {
-        this.Result(Property, Message);
+        this.Result(property, message);
       }
     }
     return this;
   }
 
-  public IsAlphanumeric(Value: any, Property: string, Message: string): Contract {
-    if (Value) {
+  public IsAlphanumeric(value: any, property: string, message: string): Contract {
+    if (value) {
       // eslint-disable-next-line prefer-regex-literals
-      const result = new RegExp("/^[a-z0-9]+$/i").test(Value);
+      const result = new RegExp("/^[a-z0-9]+$/i").test(value);
       if (result) {
-        this.Result(Property, Message);
+        this.Result(property, message);
       }
     }
     return this;
   }
 
-  public IsNumber(Value: any, Property: string, Message: string): Contract {
-    if (Value) {
-      const result = Number.isNaN(Value);
+  public IsNumber(value: any, property: string, message: string): Contract {
+    if (value) {
+      const result = Number.isNaN(value);
       if (!result) {
-        this.Result(Property, Message);
+        this.Result(property, message);
       }
     }
     return this;
   }
 
-  protected Result(Property: string, Message: string): void {
-    this.AddNotification(Property, Message);
+  protected Result(property: string, message: string): void {
+    this.AddNotification(property, message);
   }
 }
